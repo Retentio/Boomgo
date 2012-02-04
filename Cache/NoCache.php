13a5bf7ccd9c97b993255e44beb42459924367bd
Ludovic Fleury <ludo.fleury@gmail.com>
2012-02-04 05:13:17 +0100

    Fixe NoCache, improve unit test.

--- Cache/NoCache.php
+++ Cache/NoCache.php
@@ -66,6 +66,6 @@ class NoCache implements CacheInterface
      */
     public function delete($identifier)
     {
-        return true
+        return true;
     }
 }
---
+++ Tests/Units/Cache/NoCache.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * This file is part of the Boomgo PHP ODM.
+ *
+ * http://boomgo.org
+ * https://github.com/Retentio/Boomgo
+ *
+ * (c) Ludovic Fleury <ludo.fleury@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Boomgo\tests\units\Cache;
+
+use Boomgo\Cache;
+
+require_once __DIR__.'/../../../vendor/mageekguy.atoum.phar';
+require_once __DIR__.'/../../../Cache/CacheInterface.php';
+require_once __DIR__.'/../../../Cache/NoCache.php';
+
+/**
+ * NoCache Test
+ *
+ * @author Ludovic Fleury <ludo.fleury@gmail.com>
+ */
+
+class NoCache extends \mageekguy\atoum\test
+{
+    public function testSave()
+    {
+        $cache = new Cache\NoCache();
+
+        // Should return true
+        $this->assert
+            ->boolean($cache->save('identifier', 'data'))
+            ->isTrue();
+    }
+
+    public function testContains()
+    {
+        $cache = new Cache\NoCache();
+
+        // Should return false
+        $this->assert
+            ->boolean($cache->contains('identifier'))
+            ->isFalse();
+    }
+
+    public function testFetch()
+    {
+        $cache = new Cache\NoCache();
+
+        // Should return null
+        $this->assert
+            ->variable($cache->fetch('identifier'))
+            ->isNull();
+    }
+
+    public function testDelete()
+    {
+        $cache = new Cache\NoCache();
+
+        // Should return true
+        $this->assert
+            ->boolean($cache->delete('identifier'))
+            ->isTrue();
+    }
+}
