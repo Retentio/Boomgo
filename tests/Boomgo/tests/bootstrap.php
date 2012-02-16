c100b985b6f6556415b89f07110b2651cbf22c05
David Guyon <dguyon@gmail.com>
2012-02-16 12:02:06 +0100

    Fixes Travis CI autoloading troubles

--- tests/Boomgo/tests/bootstrap.php
+++ tests/Boomgo/tests/bootstrap.php
@@ -1,9 +1,12 @@
 <?php
 
-if ((!$loader = @include __DIR__.'/../../../vendor/.composer/autoload.php')) {
+if (file_exists($file = __DIR__.'/../../../vendor/.composer/autoload.php')) {
+    $loader = require $file;
+    
+    $loader->add('Boomgo\\tests', 'tests');
+    $loader->register();
+} else {
     die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
         'php composer.phar install'.PHP_EOL);
 }
-
-$loader->add('Boomgo\\tests', 'tests');
