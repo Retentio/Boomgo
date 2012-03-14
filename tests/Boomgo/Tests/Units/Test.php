6909b78991b0887e4a8910550ae64675ede7a551
Ludovic Fleury <ludo.fleury@gmail.com>
2012-03-14 12:17:01 +0100

    Updates test namespace configuration for atoum.

--- tests/Boomgo/Tests/Units/Test.php
+++ tests/Boomgo/Tests/Units/Test.php
@@ -8,7 +8,7 @@ abstract class Test extends AtoumTest
 {
     public function __construct(score $score = null, locale $locale = null, adapter $adapter = null)
     {
-        $this->setTestNamespace('Tests\Units');
+        $this->setTestNamespace('\\Tests\\Units\\');
         parent::__construct($score, $locale, $adapter);
     }
 }
