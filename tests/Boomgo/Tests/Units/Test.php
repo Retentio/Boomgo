abaefd41c06df125d7853dec5bca4cdf9bfd22c0
David Guyon <dguyon@gmail.com>
2012-04-26 09:46:43 +0200

    Update default unit test class with atoum refactoring

--- tests/Boomgo/Tests/Units/Test.php
+++ tests/Boomgo/Tests/Units/Test.php
@@ -1,14 +1,32 @@
 <?php
 
+/**
+ * This file is part of the Boomgo PHP ODM for MongoDB.
+ *
+ * http://boomgo.org
+ * https://github.com/Retentio/Boomgo
+ *
+ * (c) Ludovic Fleury <ludo.fleury@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Boomgo\Tests\Units;
 
-use mageekguy\atoum\test as AtoumTest;
+use mageekguy\atoum\test as AtoumTest,
+    mageekguy\atoum\factory;
 
+/**
+ * Rewrite default namespace for atoum
+ * 
+ * @author David Guyon <dguyon@gmail.com>
+ */
 abstract class Test extends AtoumTest
 {
-    public function __construct(score $score = null, locale $locale = null, adapter $adapter = null)
+    public function __construct(factory $factory = null)
     {
         $this->setTestNamespace('\\Tests\\Units\\');
-        parent::__construct($score, $locale, $adapter);
+        parent::__construct($factory);
     }
 }
